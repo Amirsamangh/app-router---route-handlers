f08be9dc2e06f48ce787c6951eb93ea422ce5e0d
Amirsaman <gharechahi72@gmail.com>
2025-08-09 03:06:48 +0330

    CORS

--- app/api/products/route.js
+++ app/api/products/route.js
@@ -1,10 +1,13 @@
 export async function GET(request) {
+
     const data = [
         { id: 1, title: 'product1', price: '$700' },
         { id: 2, title: 'product2', price: '$450' },
     ]
     return Response.json({ data })
 }
+
+
 export async function POST(request) {
     const data = [
         { id: 1, title: 'product1', price: '$700' },
